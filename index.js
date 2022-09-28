a39ec38996bd45b2316266e6abfed4dc007d5cb8
David Lucid <davidlucidsl@gmail.com>
2022-09-28 14:31:29 -0500

    Wrap exception text with Error

--- index.js
+++ index.js
@@ -288,8 +288,8 @@ class WhaleKeyring extends EventEmitter {
       },
     });
     if (res.data.signTransaction.r === undefined) {
-      if (res.data.signTransaction.__typename === "ErrorResponse") throw res.data.signTransaction.message;
-      throw "Unknown Kevlar API error when signing transaction.";
+      if (res.data.signTransaction.__typename === "ErrorResponse") throw new Error(res.data.signTransaction.message);
+      throw new Error("Unknown Kevlar API error when signing transaction");
     }
     res.data.signTransaction.v -= 27;
     return res.data.signTransaction;
@@ -312,8 +312,8 @@ class WhaleKeyring extends EventEmitter {
       },
     });
     if (res.data.signMessage.r === undefined) {
-      if (res.data.signMessage.__typename === "ErrorResponse") throw res.data.signMessage.message;
-      throw "Unknown Kevlar API error when signing personal message.";
+      if (res.data.signMessage.__typename === "ErrorResponse") throw new Error(res.data.signMessage.message);
+      throw new Error("Unknown Kevlar API error when signing personal message");
     }
     const rawMsgSig = concatSig(res.data.signMessage.v, res.data.signMessage.r, res.data.signMessage.s);
     return rawMsgSig;
@@ -382,8 +382,8 @@ class WhaleKeyring extends EventEmitter {
       },
     });
     if (res.data.signTypedData.r === undefined) {
-      if (res.data.signTypedData.__typename === "ErrorResponse") throw res.data.signTypedData.message;
-      throw "Unknown Kevlar API error when signing typed data.";
+      if (res.data.signTypedData.__typename === "ErrorResponse") throw new Error(res.data.signTypedData.message);
+      throw new Error("Unknown Kevlar API error when signing typed data");
     }
     return concatSig(ethUtil.toBuffer(res.data.signTypedData.v), res.data.signTypedData.r, res.data.signTypedData.s);
   }
